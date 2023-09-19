4b52e6c843f52076b666c428ad2f2c640e05a6df
Gasol Wu <gasol.wu@gmail.com>
2023-09-19 11:33:39 +0800

    refactor: Use router package instead of bundled router

--- test/app.param.js
+++ test/app.param.js
@@ -9,21 +9,16 @@ describe('app', function(){
     it('should map app.param(name, ...) logic', function(done){
       var app = express();
 
-      app.param(function(name, regexp){
-        if (Object.prototype.toString.call(regexp) === '[object RegExp]') { // See #1557
-          return function(req, res, next, val){
-            var captures;
-            if (captures = regexp.exec(String(val))) {
-              req.params[name] = captures[1];
-              next();
-            } else {
-              next('route');
-            }
-          }
+      app.param('name', function(req, res, next, value, name) {
+        var regexp = /^([a-zA-Z]+)$/;
+        var captures;
+        if (captures = regexp.exec(String(value))) {
+          req.params[name] = captures[1];
+          next();
+        } else {
+          next('route');
         }
-      })
-
-      app.param(':name', /^([a-zA-Z]+)$/);
+      });
 
       app.get('/user/:name', function(req, res){
         res.send(req.params.name);
